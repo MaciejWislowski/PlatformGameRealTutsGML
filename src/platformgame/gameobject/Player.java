3333d7a19042389578a9231bd1e31e7e81e5e5e6
Maciej Wislowski <51289577+MaciejWislowski@users.noreply.github.com>
2021-03-27 14:42:48 +0000

    Ep 7 - Collision Bounds

--- src/platformgame/gameobject/Player.java
+++ src/platformgame/gameobject/Player.java
@@ -19,7 +19,7 @@ public class Player extends GameObject {
 
     public void tick(LinkedList<GameObject> object) {
         x += velX;
-        y += velY;
+//        y += velY;
 
         if(falling || jumping) {
             velY += gravity;
@@ -34,10 +34,32 @@ public class Player extends GameObject {
     public void render(Graphics g) {
         g.setColor(Color.blue);
         g.fillRect((int)x,(int)y, (int)width, (int)height);
+
+        Graphics2D g2d = (Graphics2D) g;
+        g.setColor(Color.red);
+        g2d.draw(getBounds());
+        g2d.draw(getBoundsRight());
+        g2d.draw(getBoundsLeft());
+        g2d.draw(getBoundsTop());
+
     }
 
-
+    // Collisions
+    // Collision from down
     public Rectangle getBounds() {
-        return new Rectangle((int)x,(int)y, (int)width, (int)height );
+        return new Rectangle((int)(x+(width/2)-(width/4)),(int)(y + (height/2)), (int)width/2, (int)height/2 );
     }
+    // Collision from top
+    public Rectangle getBoundsTop() {
+        return new Rectangle((int)(x+(width/2)-(width/4)),(int)y, (int)width/2, (int)height / 2 );
+    }
+    // Collision from right
+    public Rectangle getBoundsRight() {
+        return new Rectangle((int)(x+(width*3/4)),(int)y+5, (int)width/4, (int)height - 10 );
+    }
+    // Collision from left
+    public Rectangle getBoundsLeft() {
+        return new Rectangle((int)x,(int)y+5, (int)width/4, (int)height - 10 );
+    }
+
 }

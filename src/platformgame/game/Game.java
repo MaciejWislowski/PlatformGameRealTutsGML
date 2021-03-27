ee6cbdba23b7f6d43db57b1dc542561722f81858
Maciej Wislowski <51289577+MaciejWislowski@users.noreply.github.com>
2021-03-27 11:43:37 +0000

    Ep 2 - Game Loop

--- src/platformgame/game/Game.java
+++ src/platformgame/game/Game.java
@@ -1,25 +1,71 @@
 package platformgame.game;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable {
 
     private boolean running = false;
     private  Thread thread;
 
-    private static final long serialVersionUID = 123;
-
     public synchronized void start() {
         if (running)
             return;
 
         running = true;
-        thread = new Thread();
+        thread = new Thread(this);
         thread.start();
     }
 
     public void run(){
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1_000_000_000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now-lastTime) / ns;
+            lastTime = now;
+            while(delta>=1){
+                tick();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
 
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames + " Ticks: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
+
+    }
+
+    private void tick() {
+
+    }
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        ///////////////////////////////////
+        g.setColor(Color.gray);
+        g.fillRect(0,0,800,600);
+
+        ///////////////////////////////////
+        g.dispose();
+        bs.show();
     }
 
     public static void main(String[] args) {
